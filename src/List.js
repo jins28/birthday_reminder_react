1ed65a48f952140af141d7489328f875646d08f8
Jinsmol Joy <jinssean@gmail.com>
2023-07-03 16:18:38 +0200

    first commit

---
+++ src/List.js
@@ -0,0 +1,21 @@
+import React from 'react';
+export default function List({people}) {
+
+  return (
+    <>
+      {people.map((person)=>{
+        const{id,name,age,image} = person;
+        return(
+        <article key= {id} className='person'>
+            <img src={image} alt='image of  person'/>
+            <div>
+                <h4>{name}</h4>
+                <h4>{age}</h4>
+            </div>
+        </article>
+     )
+      })}
+    </>
+  );
+};
+
